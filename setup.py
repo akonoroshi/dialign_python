391cff829727ec43916b5987c48fae0d897b721b
Yuya Asano <akr.oedo@gmail.com>
2025-02-19 14:58:42 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='dialign_python',
+    packages=find_packages(),
+    version='0.7.0',
+    description='Python implementation of Dialign',
+    author='Paras Sharma, Ava Cai, Yuya Asano',
+)
