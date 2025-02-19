b470041a32fce05c6933338b4081ae6bffe46b19
Yuya Asano <akr.oedo@gmail.com>
2025-02-19 16:50:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='dialign_python',
     packages=find_packages(),
-    version='0.7.0',
+    version='0.1.0',
     description='Python implementation of Dialign',
     author='anonymous',
 )
